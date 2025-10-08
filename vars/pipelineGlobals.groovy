346694252de9684d216eeb4a1589f2c94ee4edc2
vasanth-mvvr <veeravasanthreddy2003@gmail.com>
2025-10-08 16:51:03 +0530

    latest

---
+++ vars/pipelineGlobals.groovy
@@ -0,0 +1,14 @@
+static final string region(){
+    return "us-east-1"
+}
+
+static final string nexusURL(){
+    return "nexus.vasanthreddy.space:8081"
+}
+static final string account_ID(){
+    return "656902114881"
+}
+
+static final string environment(){
+    return "dev"
+}
